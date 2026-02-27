e0b93fda9109e9663f7c8a6b46110eb335d127bd
zscheng <2930048068@qq.com>
2026-02-27 20:03:30 +0800

    enhance: 主题颜色使用最新css语法

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import { Outlet } from 'react-router-dom'
 
 import './App.css'
-import useTheme from './hooks/common/useTheme'
-import { mouseTrail } from './utils/mouseTrail'
 
 export default function App() {
-  mouseTrail.add()
-  useTheme()
-
   return <Outlet />
 }
